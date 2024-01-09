f01fe12800df2bf7435c84f09ce5b957349e8635
Ömer Bahrioğlu <bahriogluomer@gmail.com>
2024-01-09 17:48:11 +0300

    created page files and added them to App and installed axios and cypress

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import './reset.css'
 import './App.css'
+import { Route, Switch } from 'react-router-dom'
+import HomePage from "./pages/HomePage"
+import OrderPage from "./pages/OrderPage"
+import SuccessPage from "./pages/SuccessPage"
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Switch>
+      <Route path='/' exact><HomePage/></Route>
+      <Route path='/order'><OrderPage/></Route>
+      <Route path='/success' ><SuccessPage/></Route>
+    </Switch>
     </>
   )
 }
